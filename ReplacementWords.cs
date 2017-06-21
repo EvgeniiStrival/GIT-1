895d2667804f3347a5e5b8a22356dae6455718ab
EvgeniiStrival <evgenii20126@gmail.com>
2017-06-21 22:50:31 +0300

    first commit

---
+++ ReplacementWords.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ReplacementWords
+{
+    class ReplacementWordsMain
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("Tutti-Frutti");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Tutti");
+                }
+                else  if  (i % 5 == 0)
+                {
+                    Console.WriteLine("Frutti");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
